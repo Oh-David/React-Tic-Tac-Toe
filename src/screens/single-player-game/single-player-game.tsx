1ed2f31a4c4d4bccf2c73e4467f2c4afa2285bec
David Oh <DavidOh@protonmail.com>
2022-04-14 21:53:33 -0700

    Add Audio and Haptic feedback

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,8 +1,15 @@
 import { SafeAreaView } from 'react-native'
-import React, { ReactElement, useEffect, useState } from 'react'
+import React, { ReactElement, useEffect, useState, useRef } from 'react'
 import styles from "./single-player-game.styles";
 import { GradientBackground, Board } from '@components';
-import { printFormattedBoard, BoardState, isEmpty, isFull, getAvailableMoves, isTerminal, getBestMove } from '@utils'
+import { 
+  BoardState, 
+  isEmpty, 
+  isTerminal, 
+  getBestMove } from '@utils';
+import { Audio } from 'expo-av';
+import { loadAsync } from 'expo-font';
+import * as Haptics from 'expo-haptics';
 
 export default function Game(): ReactElement {
   const [state, setState] = useState<BoardState>(
@@ -14,12 +21,21 @@ export default function Game(): ReactElement {
 
   const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
+  const popSoundRef = useRef<Audio.Sound | null>(null);
+  const pop2SoundRef = useRef<Audio.Sound | null>(null);
   const gameResult = isTerminal(state);
   const insertCell = (cell: number, symbol: "x" | "o"): void => {
     const stateCopy: BoardState = [...state];
     if(stateCopy[cell] || isTerminal(stateCopy)) return;
     stateCopy[cell] = symbol;
     setState(stateCopy);
+
+    try {
+      symbol === "x" ? popSoundRef.current?.replayAsync() : pop2SoundRef.current?.replayAsync();
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    } catch(error) {
+      console.log(error);
+    }
   }
   const handleOnCellPressed = (cell: number): void => {
     if (turn !== "HUMAN") return;
@@ -47,6 +63,25 @@ export default function Game(): ReactElement {
       }
     }
   }, [state, turn])
+
+  useEffect(() => {
+    //load sounds
+    const popSoundObject = new Audio.Sound();
+    const pop2SoundObject = new Audio.Sound();
+    const loadSounds = async () => {
+      await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
+      popSoundRef.current = popSoundObject;
+      await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
+      pop2SoundRef.current = pop2SoundObject;
+    }
+    loadSounds();
+    return () => {
+      //unload sounds
+      popSoundObject && popSoundObject.unloadAsync();
+      pop2SoundObject && pop2SoundObject.unloadAsync();
+    }
+  }, []);
+
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
