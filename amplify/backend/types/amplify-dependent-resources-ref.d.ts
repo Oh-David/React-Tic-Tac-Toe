7cfbea4619618843cc5bd223135cf70b3f783709
David Oh <DavidOh@protonmail.com>
2022-04-25 20:37:24 -0700

    add win, lose, draw sounds, and haptic feedback

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tictactoe28b6acba": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "api": {
+        "tictactoe": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,15 +1,15 @@
 import { SafeAreaView } from 'react-native'
-import React, { ReactElement, useEffect, useState, useRef } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import styles from "./single-player-game.styles";
 import { GradientBackground, Board } from '@components';
 import { 
   BoardState, 
   isEmpty, 
   isTerminal, 
-  getBestMove } from '@utils';
-import { Audio } from 'expo-av';
-import { loadAsync } from 'expo-font';
-import * as Haptics from 'expo-haptics';
+  getBestMove,
+  Cell,
+  useSounds
+} from '@utils';
 
 export default function Game(): ReactElement {
   const [state, setState] = useState<BoardState>(
@@ -21,8 +21,9 @@ export default function Game(): ReactElement {
 
   const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT");
   const [isHumanMaximizing, setIsHumanMaximizing] = useState<boolean>(true);
-  const popSoundRef = useRef<Audio.Sound | null>(null);
-  const pop2SoundRef = useRef<Audio.Sound | null>(null);
+
+  const playSound = useSounds();
+
   const gameResult = isTerminal(state);
   const insertCell = (cell: number, symbol: "x" | "o"): void => {
     const stateCopy: BoardState = [...state];
@@ -31,8 +32,7 @@ export default function Game(): ReactElement {
     setState(stateCopy);
 
     try {
-      symbol === "x" ? popSoundRef.current?.replayAsync() : pop2SoundRef.current?.replayAsync();
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+      symbol === "x" ? playSound("pop1") : playSound("pop2");
     } catch(error) {
       console.log(error);
     }
@@ -42,9 +42,36 @@ export default function Game(): ReactElement {
     insertCell(cell, isHumanMaximizing ? "x" : "o");
     setTurn("BOT");
   };
+
+  const getWinner = (winnerSymbol: Cell): "HUMAN" | "BOT" | "DRAW" => {
+    if(winnerSymbol === "x") {
+      return isHumanMaximizing ? "HUMAN" : "BOT";
+    }
+    if(winnerSymbol === "o") {
+      return isHumanMaximizing ? "BOT" : "HUMAN";
+    }
+    return "DRAW";
+  }
+
   useEffect(() => {
     if(gameResult) {
-      alert("Done");
+      const winner = getWinner(gameResult.winner);
+      switch (winner) {
+        case "HUMAN":
+          playSound("win");
+          alert ("You won");
+          return;
+        case "BOT":
+          playSound("loss");
+          alert ("You lost");
+          return;
+        case "DRAW":
+          playSound("draw");
+          alert ("You tied");
+          return;
+        default:
+          return;
+      }
     } else {
       if(turn === "BOT")
       {
@@ -63,24 +90,6 @@ export default function Game(): ReactElement {
       }
     }
   }, [state, turn])
-
-  useEffect(() => {
-    //load sounds
-    const popSoundObject = new Audio.Sound();
-    const pop2SoundObject = new Audio.Sound();
-    const loadSounds = async () => {
-      await popSoundObject.loadAsync(require("@assets/pop_1.wav"));
-      popSoundRef.current = popSoundObject;
-      await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
-      pop2SoundRef.current = pop2SoundObject;
-    }
-    loadSounds();
-    return () => {
-      //unload sounds
-      popSoundObject && popSoundObject.unloadAsync();
-      pop2SoundObject && pop2SoundObject.unloadAsync();
-    }
-  }, []);
 
   return (
     <GradientBackground>
